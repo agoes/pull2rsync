656ac0c3c4ed8874b7961f54704c395a39b49ef9
Agoes <agoes.aswari@gmail.com>
2015-04-07 00:43:27 +0700

    clone repository when documentroot is not exists

--- app/pull.php
+++ app/pull.php
@@ -11,32 +11,38 @@
 
 if (function_exists('shell_exec')) {
 	$cd = "cd " . $repo->staging->document_root;
-
+	
 	// command suffix
 	$suffix = $config['command']['_suffix'];
-	
-	// get remote host & check git host
-	$remote = trim(shell_exec($cd . " && " . $config['git']['command']['remote_url']));
 
-	// load repository config based on GET['id']
-	if (isset($_GET['id'])) {
+	if (!file_exists($repo->staging->document_root)) {
+		// clone
+		$clone = shell_exec($config['git']['command']['clone'] . " " . $repo->git . " " . $repo->staging->document_root . " " . $suffix);
+		writeLog($clone);
+	} else {
+		// get remote host & check git host
+		$remote = trim(shell_exec($cd . " && " . $config['git']['command']['remote_url']));
 
-		// pull repository
-		$cmd = shell_exec($cd . " && " . $config['git']['command']['pull'] . " " . $suffix);
+		// load repository config based on GET['id']
+		if (isset($_GET['id'])) {
 
-		// if failed to pull, write to error log
-		if ($cmd !== 'Already up-to-date.' || !stristr($cmd, 'file changed') || substr($cmd, 0, 5) == 'error') {
-			writeLog($cmd);
-		} else {
-			// succeed
-			writeLog('[complete] ' . $cmd);
-		}
+			// pull repository
+			$cmd = shell_exec($cd . " && " . $config['git']['command']['pull'] . " " . $suffix);
 
-		// set writeable directories / files
-		$deploy = json_decode(file_get_contents($deploy));
-		foreach($deploy->writeable as $i) {
-			$chmod = shell_exec($cd . " && chmod 777 " . $i);
-			writeLog($cmd);
+			// if failed to pull, write to error log
+			if ($cmd !== 'Already up-to-date.' || !stristr($cmd, 'file changed') || substr($cmd, 0, 5) == 'error') {
+				writeLog($cmd);
+			} else {
+				// succeed
+				writeLog('[complete] ' . $cmd);
+			}
+
+			// set writeable directories / files
+			$deploy = json_decode(file_get_contents($deploy));
+			foreach($deploy->writeable as $i) {
+				$chmod = shell_exec($cd . " && chmod 777 " . $i);
+				writeLog($cmd);
+			}
 		}
 	}
 
